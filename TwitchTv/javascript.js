2afb9b8241062a2c22fc0e31aebaf5b8d60d7b08
Stivi7 <Stivi7@users.noreply.github.com>
2017-08-11 22:02:19 +0200

    Final Update

--- TwitchTv/javascript.js
+++ TwitchTv/javascript.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     var twitchChannels = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
     var api0 = "https://wind-bow.glitch.me/twitch-api/streams/freecodecamp/"
@@ -6,9 +6,9 @@ $(document).ready(function(){
     var api2 = "https://wind-bow.glitch.me/twitch-api/channels/";
     var api3 = "https://wind-bow.glitch.me/twitch-api/streams/";
 
-    
-    $.getJSON(api0, function(data0){
-        if(data0.status==null){
+
+    $.getJSON(api0, function (data0) {
+        if (data0.status == null) {
             $("#fccStatus").html("<p>FreeCodeCamp is OFFLINE</p>");
         } else {
             $("#fccStatus").html("<a href = " + "https://www.twitch.tv/freecodecamp" + ">" + "<p>FreeCodeCamp is ONLINE</p></a>")
@@ -17,50 +17,107 @@ $(document).ready(function(){
 
 
 
-        $("#allChannels").click(function(){
-            for (var i = 0; i < twitchChannels.length; i++){
-                //$("#dataTable").append("<tr id='data-" + i + "'></tr>")
+    $("#allChannels").click(function () {
+
+        // Clear table when data refreshed
+        $("#dataTable").html("");
+        for (var i = 0; i < twitchChannels.length; i++) {
+            $.when(
+                $.getJSON(api1 + twitchChannels[i]),
+                $.getJSON(api2 + twitchChannels[i]),
+                $.getJSON(api3 + twitchChannels[i])
+            ).done(function (data1, data2, data3) {
+                data1 = data1[0]
+                data2 = data2[0]
+                data3 = data3[0]
                 var rowEl = $("<tr></tr>")
-                
-                $.getJSON(api1 + twitchChannels[i], function(data1){
-                    var logoTd = $("<td><img src=" + data1.logo + "></td>")
-                    rowEl.append(logoTd)
-                    //$("#data-" + i).append("<td><img src=" + data1.logo + "/></td>")    
+                // Logo
 
-                    //$("#dataLogo").html('');
-                });//logo json
+                if (!data1.logo) {
+                    rowEl.append("<td><img src='https://www.neto.com.au/assets/images/default_product.gif'></td>")
+                } else {
+                    rowEl.append("<td><img src=" + data1.logo + "></td>")
+                }
 
-               
-                $.getJSON(api2 + twitchChannels[i], function(data2){
+                // Name
+                rowEl.append("<td><a href=" + data2.url + "> <p>" + data2.display_name + "</p></a></td>");
 
-                    
-                    
-                    $("#dataChannel").append("<a href=" + data2.url + "> <p>" + data2.display_name + "</p></a>")
-                    //$("#dataChannel").html('');
-                });//channel url end
-                
-                
-                $.getJSON(api3 + twitchChannels[i], function(data3){
-
-                    
-                    
-                    if(data3.stream === null){
-                        $("#dataStatus").append("<p>OFFLINE</p>")
-                    } else {
-                        $("#dataStatus").append("<p>ONLINE</p>")
-                    }
-
-                    //$("#dataStatus").html('');
-                });//status end
+                // Status
+                if (data3.stream === null) {
+                    rowEl.append("<td><p>OFFLINE</p></td>")
+                } else {
+                    rowEl.append("<td><p>ONLINE</p></td>")
+                }
                 $("#dataTable").append(rowEl);
-            };
-        });//all click function
+            })
 
-   
+        };
+    });//all click function
+
+    $("#onlineUsers").click(function () {
+        $("#dataTable").html("");
+        for (var i = 0; i < twitchChannels.length; i++) {
+            $.when(
+                $.getJSON(api1 + twitchChannels[i]),
+                $.getJSON(api2 + twitchChannels[i]),
+                $.getJSON(api3 + twitchChannels[i])
+            ).done(function (data1, data2, data3) {
+                data1 = data1[0]
+                data2 = data2[0]
+                data3 = data3[0]
+                var rowEl = $("<tr></tr>")
+
+                if (data3.stream !== null) {
+                    if (!data1.logo) {
+                        rowEl.append("<td><img src='https://www.neto.com.au/assets/images/default_product.gif'></td>")
+                    } else {
+                        rowEl.append("<td><img src=" + data1.logo + "></td>")
+                    }//if logo
+
+                    // Name
+                    rowEl.append("<td><a href=" + data2.url + "> <p>" + data2.display_name + "</p></a></td>");
+                    rowEl.append("<td><p>ONLINE</p></td>")
+                    $("#dataTable").append(rowEl);
+                }
+
+            });
+
+        }
+    });// online users end
+
+    $("#offlineUsers").click(function () {
+        $("#dataTable").html("");
+        for (var i = 0; i < twitchChannels.length; i++) {
+            $.when(
+                $.getJSON(api1 + twitchChannels[i]),
+                $.getJSON(api2 + twitchChannels[i]),
+                $.getJSON(api3 + twitchChannels[i])
+            ).done(function (data1, data2, data3) {
+                data1 = data1[0]
+                data2 = data2[0]
+                data3 = data3[0]
+                var rowEl = $("<tr></tr>")
+
+                if (data3.stream === null) {
+                    if (!data1.logo) {
+                        rowEl.append("<td><img src='https://www.neto.com.au/assets/images/default_product.gif'></td>")
+                    } else {
+                        rowEl.append("<td><img src=" + data1.logo + "></td>")
+                    }//if logo
+
+                    // Name
+                    rowEl.append("<td><a href=" + data2.url + "> <p>" + data2.display_name + "</p></a></td>");
+                    rowEl.append("<td><p>OFFLINE</p></td>")
+                    $("#dataTable").append(rowEl);
+                }
+
+            });
+
+        }
+    });// offline users end
+
+
+
+
 
 });//end
-
-    
-
-   
-    
