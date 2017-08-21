85db7e01adddf15c4fe9b1071dee8a0495523454
Stivi7 <Stivi7@users.noreply.github.com>
2017-08-21 21:17:18 +0200

    Calculator project done

--- Calculator/javascript.js
+++ Calculator/javascript.js
@@ -4,17 +4,52 @@ $('document').ready(function () {
     operation = 0, // Records code for the operators
     maxLength = 20 // Max length of digits
 
-  $('#ac').click(function () {
+  $('#ac').click(function () { // all clear function
+    current = '0'
+    operation = 0
+    memory = '0'
     $('#screen').html('0')
   })
 
-  $('.buttonBody').click(function (e) {
-    // current = e.target.id
-    if (current == 0 /*&& current.indexOf(".") == -1*/) {
+  $('#ce').click(function () { // clear function for current value
+    current = '0'
+    $('#screen').html('0')
+  })
+  
+  $('.buttonBody').click(function (e) { // adds digits to the screen
+    
+    if (current == 0 && current.indexOf('.') == -1) {
       current = e.target.id
+      
     } else {
       current = current + e.target.id
     }
+    if (current.length > maxLength) {
+      current = 'Too Long'
+    }
+
     $('#screen').html(current)
+  })
+
+  $('.buttonDot').click(function (e) { // insert dot if nesecary
+    if (current.length == 0) {
+      current = '0.'
+    } else {
+      if ((current.indexOf('.') == -1) && (current.indexOf('e') == -1)) {
+        current = current + '.'
+      }
+    }
+    $('#screen').html(current)
+  })
+
+  
+
+  $('.buttonEqual').click(function () {
+    
+    $('#screen').html(eval(current))
+    operation = 0 // clear operation
+    memory = "0" // clear memory
+    
+    
   })
 })
